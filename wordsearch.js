eb800e71a8569544736fbc68d14b3fdb9c53b9e6
John Chia <john.chia0106@gmail.com>
2022-01-19 19:56:43 -0800

    init commit for wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (matrix) {
+    let result = [];
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (i === 0) {
+                result.push([matrix[i][j]]);
+            } else {
+                result[j].push(matrix[i][j]);
+            }
+        }
     }
-}
+    return result;
+};
 
-module.exports = wordSearch
+
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(vs => vs.join(''));
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+    for (const v of verticalJoin) {
+        if (v.includes(word)) return true;
+    }
+    return false;
+};
+
+module.exports = wordSearch;
